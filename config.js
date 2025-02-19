089151510819950165498a65c1cc46879cf99718
Satsara2008u10 <borumarisidannaepa20@gmail.com>
2025-02-19 20:55:05 +0530

    Update config.js

--- config.js
+++ config.js
@@ -6,8 +6,8 @@ function convertToBool(text, fault = "true") {
   return text === fault ? true : false;
 }
 module.exports = {
-  SESSION_ID: process.env.SESSION_ID || "m3523J4R#hFweCit7r0ggx_G2LTCvPYiRYmZSxEGL8UgGeHSIC2U",
-  OWNER_NUM: process.env.OWNER_NUM || "94763513529",
+  SESSION_ID: process.env.SESSION_ID || "HPpCjbRT#4NEbk8nE22k2JfdMreOPM26_4iVozCSko1iT92NT8bs",
+  OWNER_NUM: process.env.OWNER_NUM || "94775472083",
   PREFIX:process.env.PREFIX || ".",
   ALIVE_IMG:process.env.ALIVE_IMG||"https://raw.githubusercontent.com/NethminaPansil/Whtsapp-bot/refs/heads/main/images%20(11).jpeg",
   ALIVE_MSG:process.env.ALIVE_MSG || "Hello , I am alive now!!\n\n Join my Support group using this Link 📥 \n\n https://chat.whatsapp.com/EXPFUXpHxFR5ur03emaNx0 \n\n 💝 𝐌𝐚𝐝𝐞 𝐛𝐲 PANSILU 💝 \n\n 🔒 ᐯㄖ尺ㄒ乇乂 爪ᗪ ㄒ乇卂爪 🔒 ",
